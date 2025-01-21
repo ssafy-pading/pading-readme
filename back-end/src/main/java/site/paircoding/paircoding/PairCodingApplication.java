6b45f9c99112b7ea7a370120184efe4ebe5aec08
InJoo <jjoonior97@gmail.com>
2025-01-21 11:10:02 +0900

    :memo: docs: 백엔드 프로젝트 생성

---
+++ back-end/src/main/java/site/paircoding/paircoding/PairCodingApplication.java
@@ -0,0 +1,13 @@
+package site.paircoding.paircoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PairCodingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PairCodingApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/site/paircoding/paircoding/PairCodingApplicationTests.java
@@ -0,0 +1,13 @@
+package site.paircoding.paircoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PairCodingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
