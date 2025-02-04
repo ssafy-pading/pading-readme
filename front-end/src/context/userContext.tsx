aba2051880cd0e815161aaaaeeb7c3a2a3065c5e
Sinwoo <sinukang333@gmail.com>
2025-02-04 17:44:34 +0900

    ♻️ refactor: 유저 상태 관리 로직 변경 S12P11C202-132

--- front-end/src/context/userContext.tsx
+++ front-end/src/context/userContext.tsx
@@ -1,32 +1,57 @@
-import React, { createContext, useContext, useState, ReactNode } from 'react';
+import React, { createContext, useContext, useState, useEffect, ReactNode } from 'react';
 import { GetMyPageResponse } from '../shared/types/mypageApiResponse';
+import useMypageAxios from '../shared/apis/useMypageAxios';
 
 interface UserContextProps {
-  /* 유저 프로필 객체 */
-  userProfile: GetMyPageResponse|null;
-  setUserProfile: (object:GetMyPageResponse|null) => void;
-
-  // /** 액세스 토큰 */
-  // accessToken: string|undefined;
-  // setAccessToken: (accessToken:string) => void;
-
+  /** 유저 프로필 객체 */
+  userProfile: GetMyPageResponse | null;
+  setUserProfile: (profile: GetMyPageResponse | null) => void;
+  isAuthenticated: boolean;
 }
 
 const UserContext = createContext<UserContextProps | undefined>(undefined);
 
 export const UserProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
-  const [userProfile, setUserProfile] = useState<GetMyPageResponse|null>({
-    name:"", email:"", image:null
-  });
-  // const [accessToken, setAccessToken] = useState<string>("");
+  const [userProfile, setUserProfile] = useState<GetMyPageResponse | null>(null);
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+  const { getProfile } = useMypageAxios();
+  useEffect(() => {
+    // 1. 로그인 상태 체크 (예: JWT 토큰 확인)
+    const token = sessionStorage.getItem('accessToken'); // 혹은 sessionStorage 사용 가능
+    if (!token) {
+      setIsAuthenticated(false);
+      return; // 로그인되지 않았다면 fetchUserProfile 호출하지 않음
+    }
+
+    setIsAuthenticated(true);
+
+    // 2. 로그인된 경우 프로필 가져오기
+    if (!userProfile) {
+      fetchUserProfile();
+    }
+    console.log(userProfile);
+  }, [userProfile]);
+
+  // 백엔드에서 유저 프로필을 불러오는 함수
+  const fetchUserProfile = async () :Promise<void> => {
+    try {
+      const response:GetMyPageResponse = await getProfile();
+
+      if (!response) {
+        throw new Error('프로필 불러오기에 실패하였습니다.');
+      }
+      setUserProfile(response);
+    } catch (error) {
+      console.error('Error fetching user profile:', error);
+    }
+  };
 
   return (
     <UserContext.Provider 
       value={{ 
         userProfile,
         setUserProfile,
-        // accessToken,
-        // setAccessToken
+        isAuthenticated
       }}
     >
       {children}
