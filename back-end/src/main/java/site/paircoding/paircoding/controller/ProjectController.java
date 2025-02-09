2ed7181142c4f0790e9de8ef930c17a7906e8cc3
InJoo <jjoonior97@gmail.com>
2025-02-09 15:18:13 +0900

    :recycle: refactor: 프로젝트 옵션 컨트롤러 리팩토링

--- back-end/src/main/java/site/paircoding/paircoding/controller/ProjectController.java
+++ back-end/src/main/java/site/paircoding/paircoding/controller/ProjectController.java
@@ -7,15 +7,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import site.paircoding.paircoding.annotaion.GroupRoleCheck;
 import site.paircoding.paircoding.entity.Project;
 import site.paircoding.paircoding.entity.dto.GroupUserResponse;
 import site.paircoding.paircoding.entity.dto.ProjectCreateRequest;
-import site.paircoding.paircoding.entity.dto.ProjectLanguageDto;
-import site.paircoding.paircoding.entity.dto.ProjectOSDto;
-import site.paircoding.paircoding.entity.dto.ProjectPerformanceDto;
 import site.paircoding.paircoding.entity.enums.Role;
 import site.paircoding.paircoding.global.ApiResponse;
 import site.paircoding.paircoding.service.ProjectService;
@@ -26,24 +22,6 @@ import site.paircoding.paircoding.service.ProjectService;
 public class ProjectController {
 
   private final ProjectService projectService;
-
-  // 사용 가능한 언어 리스트 조회
-  @GetMapping("/language")
-  public ApiResponse<List<ProjectLanguageDto>> getLanguage() {
-    return ApiResponse.success(projectService.getLanguage());
-  }
-
-  // 사용 가능한 OS 리스트 조회
-  @GetMapping("/os")
-  public ApiResponse<List<ProjectOSDto>> getOS(@RequestParam String language) {
-    return ApiResponse.success(projectService.getOS(language));
-  }
-
-  // 사용 가능한 사양 리스트 조회
-  @GetMapping("/performance")
-  public ApiResponse<List<ProjectPerformanceDto>> getPerformance() {
-    return ApiResponse.success(projectService.getPerformance());
-  }
 
   // 등록 가능한 멤버 리스트 조회 - 그룹 내 프로젝트에 없는 멤버 권한 유저
   @GetMapping("/users")
@@ -65,5 +43,5 @@ public class ProjectController {
   // 그룹 내 참여중인 프로젝트 리스트 조회
 
   // 프로젝트 상세 조회
-  
+
 }
