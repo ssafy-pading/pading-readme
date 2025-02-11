3bd6e1954b9efdcc7348f13c2c6fa086ecc48043
JaeHyung <arnold714@naver.com>
2025-02-11 15:00:45 +0900

    :sparkles: Project 목록조회, 상세조회 구현 S12P11C202-268

--- back-end/src/main/java/site/paircoding/paircoding/controller/ProjectController.java
+++ back-end/src/main/java/site/paircoding/paircoding/controller/ProjectController.java
@@ -9,9 +9,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import site.paircoding.paircoding.annotaion.GroupRoleCheck;
+import site.paircoding.paircoding.annotaion.LoginUser;
 import site.paircoding.paircoding.entity.Project;
+import site.paircoding.paircoding.entity.User;
 import site.paircoding.paircoding.entity.dto.GroupUserResponse;
 import site.paircoding.paircoding.entity.dto.ProjectCreateRequest;
+import site.paircoding.paircoding.entity.dto.ProjectWithUsersResponse;
 import site.paircoding.paircoding.entity.enums.Role;
 import site.paircoding.paircoding.global.ApiResponse;
 import site.paircoding.paircoding.service.ProjectService;
@@ -41,7 +44,18 @@ public class ProjectController {
   }
 
   // 그룹 내 참여중인 프로젝트 리스트 조회
+  @GetMapping()
+  @GroupRoleCheck(Role.MEMBER)
+  public ApiResponse<List<ProjectWithUsersResponse>> getProjects(@LoginUser User user,
+      @PathVariable("groupId") Integer groupId) {
+    return ApiResponse.success(projectService.getProjects(user, groupId));
+  }
 
   // 프로젝트 상세 조회
-
+  @GetMapping("/{projectId}")
+  @GroupRoleCheck(Role.MEMBER)
+  public ApiResponse<ProjectWithUsersResponse> getProject(@LoginUser User user,
+      @PathVariable("groupId") Integer groupId, @PathVariable("projectId") Integer projectId) {
+    return ApiResponse.success(projectService.getDetailProject(user, groupId, projectId));
+  }
 }
