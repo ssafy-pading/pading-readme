23d42390739349bf5645b9b1f57e1cf10181fb14
Heewon <shinheewon0107@gmail.com>
2025-02-16 22:34:35 +0900

    :sparkles: feat: 프로젝트 언어 연동 S12P11C202-347

--- front-end/src/pages/ProjectPage.tsx
+++ front-end/src/pages/ProjectPage.tsx
@@ -314,6 +314,7 @@ function ProjectPage() {
                       <ProjectEditor
                         groupId={groupId}
                         projectId={projectId}
+                        framework={projectDetail?.project.projectImage.language}
                         fileRouteAndName={file.fileRouteAndName}
                         userName={user.name}
                       />
