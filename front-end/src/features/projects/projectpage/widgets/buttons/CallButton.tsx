c67ea8fd18aea276d43852adcd88d93e77bc09bb
Junik <zz262zz@naver.com>
2025-02-18 15:28:30 +0900

    :lipstick: style: 버튼 아래에 설명 추가 S12P11C202-391

--- front-end/src/features/projects/projectpage/widgets/buttons/CallButton.tsx
+++ front-end/src/features/projects/projectpage/widgets/buttons/CallButton.tsx
@@ -15,19 +15,28 @@ const CallButton: React.FC = () => {
     if (clicked) {
       const timer = setTimeout(() => {
         setClicked(false);
-      }, 1000); // 2초 동안 "호출완료" 텍스트 표시
+      }, 1000); // 1초 동안 "호출완료" 텍스트 표시
       return () => clearTimeout(timer);
     }
   }, [clicked]);
 
   return (
-    <button onClick={handleClick} className="p-2 focus:outline-none ml-2">
-      {clicked ? (
-        <span className="text-xs font-medium text-green-500">호출완료</span>
-      ) : (
-        <FaRegBell className="w-4 h-4" />
-      )}
-    </button>
+    <div className="relative inline-block">
+      <button
+        onClick={handleClick}
+        className="group bg-transparent border-none cursor-pointer py-1 ml-2 focus:outline-none"
+      >
+        {clicked ? (
+          <span className="text-xs font-medium text-green-500">호출완료</span>
+        ) : (
+          <FaRegBell className="w-4 h-4" />
+        )}
+        {/* 툴팁 */}
+        <span className="absolute top-full left-1/2 transform -translate-x-1/2 mt-1 px-2 py-1 bg-gray-700 text-white text-xs whitespace-nowrap rounded opacity-0 group-hover:opacity-100 transition-opacity pointer-events-none z-50">
+          Call Owner or Manager
+        </span>
+      </button>
+    </div>
   );
 };
 
