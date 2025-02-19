7ec03e9ba058d16f9cdb8b4fdd5cf54d82ebc426
Junik <zz262zz@naver.com>
2025-02-19 12:15:04 +0900

    :sparkles: feat: 호출 취소 기능 S12P11C202-406

--- front-end/src/pages/ProjectPage.tsx
+++ front-end/src/pages/ProjectPage.tsx
@@ -40,6 +40,8 @@ import { ResourceData } from "../features/projects/monitoring/types/monitoringTy
 import { GetProjectDetailsResponse } from "../shared/types/projectApiResponse";
 import ProjectSpinner from "../features/projects/projectpage/widgets/spinners/ProjectSpinner";
 import MonitoringDashboard from "../features/projects/monitoring/components/MonitoringDashboard";
+import { CallSocketProvider } from "../features/projects/projectpage/components/CallSocket";
+import CallButton from "../features/projects/projectpage/widgets/buttons/CallButton";
 
 function ProjectPage() {
   // 로딩 상태 체크
@@ -158,12 +160,14 @@ function ProjectPage() {
   const [activePanel, setActivePanel] = useState<
     "terminal" | "run" | "resource"
   >("terminal");
+  const [isRunTabInitialized, setIsRunTabInitialized] = useState<boolean>(false)
   // 터미널 실행 버튼이 눌러졌는지에 대한 상태 관리
   const [executeRunCommand, setExecuteRunCommand] = useState<boolean>(false);
   // 파일 실행 버튼 클릭시 호출 되는 함수
   const handleFileExecution = async () => {
     setActivePanel("run"); // 실행 결과 탭으로 전환
     setExecuteRunCommand(true); // 버튼을 누른 상태로 전환
+    setIsRunTabInitialized(true)
   };
 
   {
@@ -236,6 +240,13 @@ function ProjectPage() {
           </p>
           <div className="flex items-center justify-center text-[#d4d4d4] ml-5">
             <ParticipantsButton />
+          </div>
+        <div>
+            <CallSocketProvider groupId={Number(groupId)} projectId={Number(projectId)}>
+              <div className="flex items-center space-x-4">
+                <CallButton />
+              </div>
+            </CallSocketProvider>
           </div>
         </div>
         <div className="flex items-center justify-center gap-20">
@@ -382,6 +393,17 @@ function ProjectPage() {
                   {/* 상단 탭과 + 버튼 */}
                   <div className="flex bg-[#212426] h-[30px] box-border pr-2 items-center space-x-2">
                     <div className="flex flex-1 items-center space-x-2 box-border ml-4 gap-x-4 overflow-x-auto flex-grow select-none scroll">
+                      {/* Resource 탭 */}
+                      <button
+                        className={`items-center inline-flex justify-center h-full whitespace-nowrap ${
+                          activePanel === "resource"
+                            ? "border-b-2 border-b-[#3B82F6] text-white"
+                            : "text-[#858595] hover:text-white"
+                        } cursor-pointer`}
+                        onClick={() => setActivePanel("resource")}
+                      >
+                        Resource
+                      </button>
                       {/* Run 탭 */}
                       <button
                         className={`items-center inline-flex justify-center h-full whitespace-nowrap ${
@@ -396,21 +418,10 @@ function ProjectPage() {
                         Run
                       </button>
 
-                      {/* Resource 탭 */}
-                      <button
-                        className={`items-center inline-flex justify-center h-full whitespace-nowrap ${
-                          activePanel === "resource"
-                            ? "border-b-2 border-b-[#3B82F6] text-white"
-                            : "text-[#858595] hover:text-white"
-                        } cursor-pointer`}
-                        onClick={() => setActivePanel("resource")}
-                      >
-                        Resource
-                      </button>
                       {/* Terminal 탭 */}
                       {activePanel !== "terminal" && (
                         <button
-                          className={`items-center inline-flex justify-center h-full whitespace-nowrap text-white cursor-pointer`}
+                          className={`items-center inline-flex justify-center h-full whitespace-nowrap text-[#858595] hover:text-white cursor-pointer`}
                           onClick={() => setActivePanel("terminal")}
                         >
                           Terminal
@@ -488,8 +499,8 @@ function ProjectPage() {
                         groupId={groupId}
                         projectId={projectId}
                         runCommand={projectDetail?.project.runCommand}
-                        mode="run"
                         executeRunCommand={executeRunCommand}
+                        isRunTabInitialized={isRunTabInitialized}
                         onRunCommandExecuted={() => setExecuteRunCommand(false)}
                       />
                     </div>
@@ -504,6 +515,8 @@ function ProjectPage() {
                       <MonitoringDashboard
                         data={monitoringDataList}
                         height={terminalHeight - 30}
+                        cpuDescription={projectDetail?.project.performance.cpuDescription}
+                        memoryDescription={projectDetail?.project.performance.memoryDescription}
                       />
                     </div>
                     {/* 터미널 패널 */}
--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -1,13 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode : 'class', // tailwind의 다크모드
-  
+  darkMode: 'class', // tailwind의 다크모드
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        ring: {
+          '0%': { transform: 'rotate(0deg)' },
+          '25%': { transform: 'rotate(30deg)' },
+          '50%': { transform: 'rotate(0deg)' },
+          '75%': { transform: 'rotate(-30deg)' },
+          '100%': { transform: 'rotate(0deg)' },
+        },
+      },
+      animation: {
+        ring: 'ring 1s linear infinite',
+      },
+    },
   },
   plugins: [],
 }
