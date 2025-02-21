9ff0c8a518b835c49e3ca471e98cc9a6ae8fbaa7
InJoo <jjoonior97@gmail.com>
2025-02-21 10:06:35 +0900

    :memo: docs: exec 폴더 정리

--- exec/project_image.sql
+++
@@ -1,3 +0,0 @@
-insert into pading.project_image (tag, default_run_command, language, os, port)
-values  ('ubuntu-nodejs', 'npm start', 'NodeJS', 'Ubuntu 20.04 LTS', 3000),
-        ('ubuntu-spring', './gradlew bootrun', 'SpringBoot', 'Ubuntu 20.04 LTS', 8080);
